f0f10031d4be5180019b3169f5953eba1da561ea
Fabian Cotic <fabian.cotic@actico.com>
2022-05-02 20:01:10 +0200

    - update

--- app.py
+++ app.py
@@ -1,12 +1,58 @@
-import telegram_bot
+# import telegram_bot
+#
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def dynamic_page():
+#     return telegram_bot.main()
+#
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port='8000', debug=True)
 
-from flask import Flask
+
+from flask import Flask, request
+import telegram
+
+
+global bot
+global TOKEN
+with open("token.txt") as file:
+    token = file.read()
+bot = telegram.Bot(token=token)
 
 app = Flask(__name__)
 
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+
+    response = get_response(text)
+    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+
+    return 'ok'
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
 @app.route('/')
-def dynamic_page():
-    return telegram_bot.main()
+def index():
+    return '.'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8000', debug=True)
+    app.run(threaded=True)
