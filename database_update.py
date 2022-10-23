9eb72595a4fa17f589e945183418c059208ff1f1
Fabian Cotic <fabian.cotic@actico.com>
2022-10-23 14:25:34 +0200

    database

--- database_update.py
+++ database_update.py
@@ -14,11 +14,27 @@ def insert_row(timestamp, hashrate_in_phs, daily_reward, hoster, pool, miner):
     cursor.execute(query)
     cnxn.commit()
 
+def insert_row_prices(timestamp, open, high, low, close):
+    cnxn = pyodbc.connect(
+        'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cursor = cnxn.cursor()
+    query = "INSERT INTO BTCPRICES(timestamp, btc_day_open_price_usd, btc_day_high_price_usd, btc_day_low_price_usd, btc_day_close_price_usd) VALUES ('"+str(timestamp)+"','" + str(open)+"','"+str(high)+"','"+str(low)+"','"+str(close)+"')"
+    cursor.execute(query)
+    cnxn.commit()
+
 def select_row(timestamp, hoster, pool,miner):
     cnxn = pyodbc.connect(
         'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
     cursor = cnxn.cursor()
     cursor.execute("SELECT * FROM POOLDATA WHERE timestamp='" + timestamp + "' AND hoster='" + hoster + "' AND pool='"+ pool + "' AND miner='" + miner + "'")
+    row = cursor.fetchall()
+    return row
+
+def select_row_prices(timestamp):
+    cnxn = pyodbc.connect(
+        'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT * FROM BTCPRICES WHERE timestamp='" + str(timestamp) + "'")
     row = cursor.fetchall()
     return row
 
@@ -36,12 +52,26 @@ def import_data_from_csv():
     for index, row in df.iterrows():
         insert_row(str(row['timestamp']), str(row['hashrate_in_phs']), str(row['daily_reward']), str(row['hoster']), str(row['pool']), str(row['miner']))
 
+def import_data_from_csv_prices():
+    etl.get_file_from_azure("btc_prices.csv")
+    df = pd.read_csv('btc_prices.csv', index_col=False)
+
+    for index, row in df.iterrows():
+        insert_row_prices(str(row['timestamp']), str(row['btc_day_open_price_usd']), str(row['btc_day_high_price_usd']), str(row['btc_day_low_price_usd']), str(row['btc_day_close_price_usd']))
 
 def update(id,timestamp, hashrate_in_phs, daily_reward, hoster, pool, miner):
     cnxn = pyodbc.connect(
         'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
     cursor = cnxn.cursor()
     query = "UPDATE POOLDATA SET timestamp = '" + timestamp + "', hashrate_in_phs = '" + hashrate_in_phs + "', daily_reward = ' " + daily_reward + " ', hoster = ' " + hoster + " ', pool = ' " + pool + " ', miner = ' " + miner + " ' WHERE id = " + id
+    cursor.execute(query)
+    cnxn.commit()
+
+def update_prices(id,timestamp, open, high, low, close):
+    cnxn = pyodbc.connect(
+        'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cursor = cnxn.cursor()
+    query = "UPDATE BTCPRICES SET timestamp = '" + str(timestamp) + "', btc_day_open_price_usd = '" + str(open) + "', btc_day_high_price_usd = ' " + str(high) + " ', btc_day_low_price_usd = ' " + str(low) + " ', btc_day_close_price_usd = ' " + str(close) + " ' WHERE id = " + id
     cursor.execute(query)
     cnxn.commit()
 
@@ -68,6 +98,22 @@ def db_trim():
 
     cursor.execute(query1 + query2 + query3 + query4 + query5 + query6)
     cnxn.commit()
+
+def btc_prices():
+    from datetime import date
+    today_date = date.today().strftime('%d-%m-%Y')
+    yesterday_date = yesterday(today_date, False)
+    yesterday_date = yesterday_date.strftime('%d-%m-%Y')
+    df = etl.get_price_for_date(yesterday_date,yesterday_date)
+
+    row = select_row_prices(df['Date'].iloc[0])
+    if not row:
+        insert_row_prices(df['Date'].iloc[0], df['Open'].iloc[0], df['High'].iloc[0],df['Low'].iloc[0], df['Close'].iloc[0])
+    else:
+        id = str(row[0].id)
+        update_prices(id, df['Date'].iloc[0], df['Open'].iloc[0], df['High'].iloc[0],df['Low'].iloc[0], df['Close'].iloc[0])
+
+    return df
 
 def daily_update():
     from datetime import date
@@ -113,6 +159,8 @@ def daily_update():
 
 if __name__ == '__main__':
     daily_update()
+    btc_prices()
+
 
 
 
